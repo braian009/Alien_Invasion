1605c1a5269585ad6627f2659da11e645230ba23
braian <braianyanquin@gmail.com>
2022-06-07 22:10:53 -0300

    Initial commit

---
+++ settings.py
@@ -0,0 +1,12 @@
+# alien_invasion/settings.py
+
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+
+        # Screen settings
+        self.screen_width = 1280
+        self.screen_height = 720
+        self.bg_color = (230, 230, 230)
