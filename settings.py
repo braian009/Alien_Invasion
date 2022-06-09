16d40c9c20f52a9d22a79d689b3b6d3a9906f15b
braian <braianyanquin@gmail.com>
2022-06-08 23:02:48 -0300

    Added ship's movement and bullets

--- settings.py
+++ settings.py
@@ -10,3 +10,12 @@ class Settings:
         self.screen_width = 1280
         self.screen_height = 720
         self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed = 1.5
+
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
