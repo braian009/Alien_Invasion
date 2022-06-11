4ec518c1b3e1147bd0fdb8395f16b1659c65d5f0
braian <braianyanquin@gmail.com>
2022-06-10 23:22:28 -0300

    Alien's movement added

--- settings.py
+++ settings.py
@@ -7,8 +7,8 @@ class Settings:
         """Initialize the game's settings."""
 
         # Screen settings
-        self.screen_width = 1280
-        self.screen_height = 720
+        self.screen_width = 1200
+        self.screen_height = 700
         self.bg_color = (230, 230, 230)
 
         # Ship settings
@@ -20,3 +20,9 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+
+        # Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet direction of 1 represents right, -1 represents left.
+        self.fleet_direction = 1
