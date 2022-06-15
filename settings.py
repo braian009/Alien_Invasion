b4bce2a617ec5a46942c5718711987de6c69843f
braian <braianyanquin@gmail.com>
2022-06-15 00:02:26 -0300

    Scoring func added

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ class Settings:
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_width = 3
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
@@ -25,6 +25,9 @@ class Settings:
 
         # How quickly the game speeds up
         self.speedup_scale = 1.1
+
+        # How quicly the alien point values increase
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -37,8 +40,13 @@ class Settings:
         # fleet_direction fo 1 represents right; -1 represents left.
         self.fleet_direction = 1
 
+        # Scoring
+        self.alien_points = 50
+
     def increase_speed(self):
-        """Increase speed settings."""
+        """Increase speed settings and alien point values."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
